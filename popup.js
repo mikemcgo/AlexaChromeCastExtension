61391845e1bcde406bf881434b940c7e588a1ac6
Mike McGookey <mike.mcgookey@gmail.com>
2017-02-10 09:47:35 -0700

    Cleaned up save logic, preparing to implement background processes

--- popup.js
+++ popup.js
@@ -25,26 +25,25 @@ function getCurrentTabUrl(callback) {
 	});
 }
 
-function renderDoc(statusText) {
-	document.getElementById('status').textContent = statusText;
+function renderDoc() {
 	chrome.storage.local.get('machineName', function(result) {
 		document.getElementById('showMachineName').textContent = "Computer Name: " + result.machineName;
 	});
+	chrome.storage.local.get('targetIP', function(result) {
+		document.getElementById('showTargetIP').textContent = "Target IP: " + result.targetIP;
+	});
 }
 
-function saveName() {
-	var computerName = document.getElementById('machineName').value;
-	chrome.storage.local.set({'machineName': computerName}, function() {
+function save(key, outputElement, printString) {
+	var value = document.getElementById(key).value;
+	chrome.storage.local.set({key: value}, function() {
 		// Notify that we saved.
-		console.log(computerName + "saved")
+		document.getElementById(outputElement).textContent = printString + value
 	});
-	document.getElementById('showMachineName').textContent = "Computer Name: " + computerName
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-	document.getElementById("nameSubmit").addEventListener("click", saveName);
-	getCurrentTabUrl(function(url) {
-		// Put the image URL in Google search.
-		renderDoc('Finding ' + url);
-	});
+	document.getElementById("nameSubmit").addEventListener("click", function() {save('machineName', 'showMachineName', "Computer Name: ")});
+	document.getElementById("IPSubmit").addEventListener("click", function() {save('targetIP', 'showTargetIP', "Target IP: ")});
+	renderDoc();
 });
