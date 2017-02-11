2c2e44fb9f2218096577d6e0be0b53023f16aff7
Mike McGookey <mike.mcgookey@gmail.com>
2017-02-10 20:21:25 -0700

    ajax requests not working

--- eventPage.js
+++ eventPage.js
@@ -1,17 +1,42 @@
-var services = ["youtube", "twitch"]
+var services = ["youtube.com", "twitch.tv"]
 
 chrome.tabs.onUpdated.addListener(function(tabID, changeInfo, tab) {
 	if (changeInfo.url) {
-		var serviceFound = false
+		var serviceFound = ""
 		// Check the page for being a part of selected services
 		for (i = 0; i < services.length; i++) {
 			if (changeInfo.url.includes(services[i])) {
-				serviceFound = true
+				serviceFound = services[i]
 			}
 		}
-		if (!serviceFound) {
+		if (serviceFound === "") {
 			return 
 		}
-		alert(changeInfo.url)
+		var content = changeInfo.url.substring(changeInfo.url.search(serviceFound) + serviceFound.length + 1);
+		if (content.length != 0 && content.search('/') == -1)
+		{
+			// save to local
+			chrome.storage.local.set({serviceFound: content})
+			// Transmit to server
+			chrome.storage.local.get(function(result) {
+				var uRL = "http://" + result.targetIP + ":5000/reporter/" + result.machineName
+				console.log(uRL)
+				$.ajax({
+					type: 'POST',
+					url: uRL,
+					data: JSON.stringify({
+						serviceFound: content,
+					}),
+					dataType: 'json',
+					success: function() {
+						console.log("Put Complete");
+					},
+					error: function() {
+						// may want to create notification to encourage user to turn off sync
+						console.log("Put to server failed");
+					}
+				});
+			});
+		}
 	}
 })
